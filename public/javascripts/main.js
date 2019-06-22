37052f97ef3adab5e60976853a963035d16106c7
Dor personal <dortaldt@gmail.com>
2019-06-22 13:57:38 +0200

    added a counter to home page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -363,6 +363,38 @@ $(document).ready(function(){
     })
 });
 
+// main page counter
+
+// Set the date we're counting down to
+var countDownDate = new Date("jul 12, 2019 07:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  $("#counter-days").html(days);
+  $("#counter-hours").html(hours);
+  $("#counter-mins").html(minutes);
+  $("#counter-secs").html(seconds);
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    $("#counter-container").toggle();
+  }
+}, 1000);
 
 
 
