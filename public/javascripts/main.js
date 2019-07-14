9da7aac7090a0c26a5e7080e0e910fb17607ca25
Dor personal <dortaldt@gmail.com>
2019-07-14 13:26:44 +0200

    fix teams graph

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -162,17 +162,43 @@ $( document ).ready(function() {
 	  }
 	}
 
-	var flipSorting = function(){
-		if(flip) flip=0;
-		else flip=1
-		sortTable(flip)
-	};
-
-	$('#balance-col').click(function(event) {
-		flipSorting()
-		console.log(flip)
-	});
-
+	var sortTeamTable = function() {
+		var flip = 1;
+		var table, rows, switching, i, x, y, shouldSwitch;
+		table = document.getElementById("team-stats");
+		switching = true;
+		/*Make a loop that will continue until
+		no switching has been done:*/
+		while (switching) {
+		  //start by saying: no switching is done:
+		  switching = false;
+		  rows = table.rows;
+		  /*Loop through all table rows (except the
+		  first, which contains table headers):*/
+		  for (i = 1; i < (rows.length - 1); i++) {
+			//start by saying there should be no switching:
+			shouldSwitch = false;
+			/*Get the two elements you want to compare,
+			one from current row and one from the next:*/
+			x = rows[i].getElementsByTagName("td")[0];
+			y = rows[i + 1].getElementsByTagName("td")[0];
+			//check if the two rows should switch place:
+			var a = Number(x.innerHTML)
+			var b = Number(y.innerHTML)
+			if (a > b) {
+				//if so, mark as a switch and break the loop:
+				shouldSwitch = true;
+				break;
+			}
+		  }
+		  if (shouldSwitch) {
+			/*If a switch has been marked, make the switch
+			and mark that a switch has been done:*/
+			rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+			switching = true;
+		  }
+		}
+	  }
 
 //dealing with time formating
 	$('.report-time').each(function(index, el) {
@@ -318,7 +344,8 @@ $( document ).ready(function() {
 
 
 	console.log( "ready!" );
-	sortTable()
+	sortTable();
+	sortTeamTable();
 
 });
 
